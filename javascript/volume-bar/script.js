325b1d552365320ff2bb2e7d7932b0bbf05063fc
AliFoxExamples <48414381+AliFoxExamples@users.noreply.github.com>
2019-04-20 23:04:24 +0300

    Volume+update

--- javascript/volume-bar/script.js
+++ javascript/volume-bar/script.js
@@ -1,203 +1,105 @@
-var button = document.getElementById("button"); // Ползунок.
-var rang = document.getElementById("rang"); // Поле движения ползунка.
-var box0 = document.getElementById("box0");
-var box = document.getElementById("box1"); // Табло вывода 1.
-var box2 = document.getElementById("box2"); // Табло вывода 2.
-var box3 = document.getElementById("box3");
-var box4 = document.getElementById("box4");
-var buttonLeftOffset; // Число левого отступа ползунка от контейнера.
+var button = document.getElementById("button");				// Определение ползунока громкости.
+var range = document.getElementById("range");				// Определение поля движения ползунка громкости.
+
+var volumeBox = document.getElementById("volumeBox");				// Табло вывода результата значения громкости.
+var calculatedButtonPositionBox = document.getElementById("calculatedButtonPositionBox");				// Табло вывода значения вычисленной позиции курсора.
+var leftOffsetOfButtonBox = document.getElementById("leftOffsetOfButtonBox");				// Табло вывода значения левого отступа кнопки громкости от контейнера.
+var xCoordinateOfMouseBox = document.getElementById("xCoordinateOfMouseBox");				// Табло вывода значения координаты "x" курсора мыши на экране.
+var positionOnClickBox = document.getElementById("positionOnClickBox");				// Табло вывода значения координаты "x" курсора мыши на экране в момент клика.
+var dynamicPositionOfMouseBox = document.getElementById("dynamicPositionOfMouseBox");				// Табло вывода динамического значения координаты "x" курсора мыши на экране после клика.
+var mouseClickPositionInRangeBox = document.getElementById("mouseClickPositionInRangeBox");				// Табло вывода значения позиции курсора мыши в поле движения ползунка громкости.
 
 
+var leftOffsetOfButton;				// Значение левого отступа ползунка от контейнера.
 
-// Основной скрипт:
+var minValueOfRange = 0;				// Минимальное значение длины поля движения ползунка громкости.
+var maxValueOfRange = 183;				// Максимальное значение длины поля движения ползунка громкости.
 
 
-rang.addEventListener('mousedown', function(event) {
+range.addEventListener('mousedown', function(event) {				// Основная функция.
 
-	button.style.backgroundColor = "#006400";
-	rang.style.backgroundColor = "#80755c";
-	buttonLeftOffset = button.offsetLeft;
-	rangLeftOffset = rang.offsetLeft;
-	positionOnClick = event.clientX;
-	calculatedPosition = (positionOnClick - rangLeftOffset) - 10;
-	button.style.left = calculatedPosition + "px";
 	clickStatus = true;
 
+	button.style.backgroundColor = "#006400";
+	range.style.backgroundColor = "#80755c";
+	leftOffsetOfButton = button.offsetLeft;
+	leftOffsetOfRange = range.offsetLeft;
+	mousePositionOnClick = event.clientX;
+	mouseClickPositionInRange = (mousePositionOnClick - leftOffsetOfRange) - 10;
+
+
+	if (mouseClickPositionInRange < minValueOfRange) {				// Ограничение перемещения ползунка влево.
+		mouseClickPositionInRange = minValueOfRange;
+	}
+
+	if (mouseClickPositionInRange > maxValueOfRange) {				// Ограничение перемещения ползунка вправо.
+		mouseClickPositionInRange = maxValueOfRange;
+	}
+
+
+	button.style.left = mouseClickPositionInRange + "px";
+	
+
 	document.addEventListener('mousemove', function(event) {
 
+		if (clickStatus == true) {				// Определение статуса клика мыши.
 
+			dynamicPositionOfMouse = event.clientX;				// Позиция перемещенного курсора мыши по оси "X", после клика.
 
-
-
-
-		if (clickStatus == true) {		
-
-			dynamicPosition = event.clientX; // Позиция перемещенного курсора мыши по оси X, после клика.
-			if (dynamicPosition < 0) {
-				dynamicPosition = 0;
+			if (dynamicPositionOfMouse < minValueOfRange) {				// Ограничение перемещения ползунка влево.
+				dynamicPositionOfMouse = minValueOfRange;
 			}
 
-			if (positionOnClick > dynamicPosition ) {
+			if (mousePositionOnClick < dynamicPositionOfMouse) {
 
-				calculatedPosition = calculatedPosition + ((1 - (positionOnClick - dynamicPosition)) - 1); // Обратить число позиции курсора мыши в отрицательное, если оно левее позиции курсора мыши в момент клика.
+				calculatedButtonPosition = mouseClickPositionInRange + (dynamicPositionOfMouse - mousePositionOnClick);
+
+
+				if (calculatedButtonPosition > maxValueOfRange) {				// Ограничение перемещения ползунка вправо.
+					calculatedButtonPosition = maxValueOfRange;
+				}
+			}
+
+			else {
+
+				calculatedButtonPosition = mouseClickPositionInRange + ((1 - (mousePositionOnClick - dynamicPositionOfMouse)) - 1);				// Обращение числа позиции курсора мыши в отрицательное, если оно левее позиции курсора мыши в момент клика.
 				
 
-				if (calculatedPosition < -1) {  // Ограничение перемещения влево.
-					calculatedPosition = -1;
-					//dynamicPosition = positionOnClick;
+				if (calculatedButtonPosition < minValueOfRange) {  // Ограничение перемещения влево.
+					calculatedButtonPosition = minValueOfRange;
 
 				}
 			}
 
-			else {
-
-				calculatedPosition = calculatedPosition + (dynamicPosition - positionOnClick);
-
-
-				if (calculatedPosition > 184) {  // Ограничение перемещения вправо.
-					calculatedPosition = 184;
-					//dynamicPosition = positionOnClick;
-				}
-			}
-
-
-
-
-
-
-
-			positionOnClick = dynamicPosition;
-			button.style.left = calculatedPosition + "px";
-			box.innerHTML = "Calculated Mouse Position : " + calculatedPosition;
-			box2.innerHTML = "Left Offset : " + button.offsetLeft;
-			box3.innerHTML = "Mouse x : " + event.clientX;
-			box4.innerHTML = "Position on Click : " + positionOnClick;
-			box5.innerHTML = "Dynamic Position : " + dynamicPosition;
-
-
-
-
-
-			volume = calculatedPosition;
-			if (volume < 0) {
-				volume = 0;
-			}
-			volume = Math.floor(100 * (volume / 184));
-			box0.innerHTML = "Volume: " + volume;
-		}
-
-	});
-
-	window.addEventListener('mouseup', function(event) {
-	button.style.backgroundColor = "#3CB371";
-	rang.style.backgroundColor = "#b0a792";
-	clickStatus = false;
-
-	});
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-button.addEventListener('mousedown', function(event) {
-
-	button.style.backgroundColor = "#006400";
-	rang.style.backgroundColor = "#80755c";
-	positionOnClick = event.clientX;  // Позиция курсора мыши по оси X, в момент клика.
-	buttonLeftOffset = button.offsetLeft;  // Текущий левый отступ ползунка от контейнера.
-	var clickStatus = true;
-
-	document.addEventListener('mousemove', function(event) {
-
-		if (clickStatus == true) {		
-
-			dynamicPosition = event.clientX; // Позиция перемещенного курсора мыши по оси X, после клика.
-
-			if (positionOnClick > dynamicPosition ) {
-
-				calculatedPosition = (1 - (positionOnClick - dynamicPosition)) - 1; // Обратить число позиции курсора мыши в отрицательное, если оно левее позиции курсора мыши в момент клика.
-				calculatedPosition = buttonLeftOffset + calculatedPosition;
-
-				if (calculatedPosition < -1) {  // Ограничение перемещения влево.
-					calculatedPosition = -1;
-				}
-			}
-
-			else {
-
-				calculatedPosition = dynamicPosition - positionOnClick;
-				calculatedPosition = buttonLeftOffset + calculatedPosition;
-
-				if (calculatedPosition > 184) {  // Ограничение перемещения вправо.
-					calculatedPosition = 184;
-				}
-			}
-
-			button.style.left = calculatedPosition + "px";
-			box.innerHTML = "Calculated Mouse Position : " + calculatedPosition;
-			box2.innerHTML = "Left Offset : " + button.offsetLeft;
-
+			button.style.left = calculatedButtonPosition + "px";
+
+			volume = Math.floor(100 * (calculatedButtonPosition / maxValueOfRange));
+
+			// Вывод значений.
+
+			volumeBox.innerHTML = "Volume: " + volume;
+			calculatedButtonPositionBox.innerHTML = "Calculated Button Position : " + calculatedButtonPosition;
+			leftOffsetOfButtonBox.innerHTML = "Left Offset of Button : " + button.offsetLeft;
+			xCoordinateOfMouseBox.innerHTML = "Mouse x : " + event.clientX;
+			positionOnClickBox.innerHTML = "Position on Click : " + mousePositionOnClick;
+			dynamicPositionOfMouseBox.innerHTML = "Dynamic Position of Mouse: " + dynamicPositionOfMouse;
+			mouseClickPositionInRangeBox.innerHTML = "Mouse Click Position in Range : " + mouseClickPositionInRange;
 		}
 	});
 
-	window.addEventListener('mouseup', function(event) {
+	volume = Math.floor(100 * (mouseClickPositionInRange / (maxValueOfRange)));
+
+	// Вывод значений.
+
+	volumeBox.innerHTML = "Volume: " + volume;
+	leftOffsetOfButtonBox.innerHTML = "Left Offset of Button : " + button.offsetLeft;
+	xCoordinateOfMouseBox.innerHTML = "Mouse x : " + event.clientX;
+	positionOnClickBox.innerHTML = "Position on Click : " + mousePositionOnClick;
+	mouseClickPositionInRangeBox.innerHTML = "Mouse Click Position in Range : " + mouseClickPositionInRange;	
+
+	window.addEventListener('mouseup', function(event) {				// Прекрашение выполнения функции после отжатия кнопки мыши.
 		button.style.backgroundColor = "#3CB371";
-		rang.style.backgroundColor = "#b0a792";
+		range.style.backgroundColor = "#b0a792";
 		clickStatus = false;
 	});
-
 });
-
-*/
-
-
