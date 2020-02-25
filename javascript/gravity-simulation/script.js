f43bc452b3388b7705126902e6da7fbd1fb1dbcb
AliFoxExamples <48414381+AliFoxExamples@users.noreply.github.com>
2020-02-26 00:48:58 +0300

    javascript\gravity-simulation
    Added functionality

--- javascript/gravity-simulation/script.js
+++ javascript/gravity-simulation/script.js
@@ -1,55 +1,56 @@
-var ball = document.getElementById("ball");
-
-var ballTopOffset = ball.offsetTop;
-
-var ballAccelerationBox = document.getElementById("ballAcceleration");
-var ballTopOffsetBox = document.getElementById("ballTopOffset");
+const startButton = document.getElementById("start");
+const ball1 = document.getElementById("ball1");
 
 
-ballAcceleration = 0;
 
-var gravity = function() {
 
-	ballTopOffset = ball.offsetTop;
+let physObjects = {
+	objectSuitableWithGravityEngine: true,
+	physBox: (document.getElementById("physBox")),
+	obj1: [ball1, {elasticity : 2, taken: false}],
+	obj2: [ball2, {elasticity : 1, taken: false}],
+	obj3: [ball3, {elasticity : 0.7, taken: false}]
+}
 
-	if (ballTopOffset < 747) {
 
-		ballAcceleration = ballAcceleration + 0.1;
 
-		ballTopOffset = Math.floor(ballTopOffset + ballAcceleration);
+function applyGravityEngineTo (physObjects, physObjectsTarget) {
 
-		if (ballTopOffset > 747) {
-			ballTopOffset = 747;
+	if (!physObjects || !physObjectsTarget) {return console.error("Missing arguments!")}
+	if (!(physObjects instanceof Object) || !physObjects.objectSuitableWithGravityEngine) {return console.error("Invalid input!")}
+	if (!(physObjectsTarget in physObjects)) {return console.error("Requested phys object does not exist!")}
+	let [physObj, {elasticity : physObjectsTargetElasticity, taken : status}] = physObjects[physObjectsTarget];
+	if (status) {return console.error("Requested phys object is already taken!")}
+	physObjects[physObjectsTarget][1].taken = true;
+
+
+	let physObjectsTargetAcceleration = 0;
+	let yVelocity = 0;
+	let lastYVelocity;
+	let yMaxRange = physObjects.physBox.offsetHeight - physObjects[physObjectsTarget][0].offsetHeight -4;
+
+
+	function gravity () {
+		physObjectsTargetAcceleration += 0.1;
+		yVelocity += physObjectsTargetAcceleration;	
+		if (yVelocity >= yMaxRange) {
+			physObjectsTargetAcceleration = -physObjectsTargetAcceleration + physObjectsTargetElasticity;
+			yVelocity = yMaxRange;
+
+			if ((lastYVelocity - yVelocity) == 0) {
+				clearInterval(gravityInterval);
+				physObjects[physObjectsTarget][1].taken = false;
+			}
 		}
-
+		physObjects[physObjectsTarget][0].style  = `top: ${yVelocity}px`;
+		lastYVelocity = yVelocity;
+		yVelocity = Math.round(yVelocity);
 	}
-
-	/*
-
-	else {
-
-		ballAcceleration = -ballAcceleration + 0.1;
-
-		ballTopOffset = Math.floor(ballTopOffset + ballAcceleration);
-
-		
-	}*/
-
-	if (ballTopOffset <= 0) {
-			ballTopOffset = 0;
-		}
-
-	ball.style = "top: " + ballTopOffset + "px";
-
-	ballAccelerationBox.innerHTML = "Ball Acceleration = " + ballAcceleration;
-	ballTopOffsetBox.innerHTML = "Ball Top Offset = " + ballTopOffset;
-	
+	let gravityInterval = setInterval(gravity, 5);
 }
 
-var gravityIntervalStart = function () {
-	gravityInterval = setInterval(gravity, 10);
+function applyGravityEngineToAllPhysObjects () {
+	applyGravityEngineTo(physObjects, 'obj1');
+	applyGravityEngineTo(physObjects, 'obj2');
+	applyGravityEngineTo(physObjects, 'obj3');
 }
-
-
-
-var startButton = document.getElementById("start");
