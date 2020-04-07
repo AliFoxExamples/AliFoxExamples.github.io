b67695bfb174dbc90f596e0936db2de84054e056
AliFoxExamples <48414381+AliFoxExamples@users.noreply.github.com>
2020-04-07 15:48:14 +0300

    frnt-end\mini
    +update

--- frnt-end/mini/js/custom.js
+++ frnt-end/mini/js/custom.js
@@ -1,4 +1,23 @@
 $(function(){
+
+
+	let menuToggleStatus = false;
+	$('.menu-toggle').click(function() {
+		if (!menuToggleStatus) {
+			$(this).css({"transform" : "rotate(90deg)"});
+			menuToggleStatus = true;
+		}
+		else {
+			$(this).css({"transform" : "rotate(0deg)"});
+			menuToggleStatus = false;
+		}
+			
+		$('.main-menu').slideToggle();
+	})
+
+
+
+
 	$('.tabs a').click(function(){
 		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
 		$(this).parent().siblings().removeClass('active');
@@ -7,7 +26,20 @@ $(function(){
 		$(this).parent().addClass('active');
 		return false
 	});
+
+	$(".banner-slider, .testimonial-slider").slick({
+		arrows: false,
+		dots: true
+	});
+	$(".portfolio-slider").slick({
+		dots: true,
+		appendArrows: ".portfolio-slider__buttons",
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-right"></i></button>',
+	});
 });
+
+
 
    
 
