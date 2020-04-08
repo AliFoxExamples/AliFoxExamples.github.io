676d35648bcf87e6a0c5d8fa937bf42589c93b98
Ali-Fox-Examples <user-ali-sadykov@yadex.ru>
2020-04-08 23:05:58 +0300

    Mini +update
    Responsibility

--- frnt-end/mini/js/custom.js
+++ frnt-end/mini/js/custom.js
@@ -21,7 +21,7 @@ $(function(){
 	$('.tabs a').click(function(){
 		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
 		$(this).parent().siblings().removeClass('active');
-		var id = $(this).attr('href');
+		let id = $(this).attr('href');
 		$(id).removeClass('hide');
 		$(this).parent().addClass('active');
 		return false
@@ -31,12 +31,50 @@ $(function(){
 		arrows: false,
 		dots: true
 	});
+
 	$(".portfolio-slider").slick({
 		dots: true,
 		appendArrows: ".portfolio-slider__buttons",
-		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+		prevArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-left"></i></button>',
 		nextArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-right"></i></button>',
+		responsive: [
+		    {
+		      breakpoint: 800,
+		      settings: {
+		        dots: false
+		      }
+		    }
+		]
 	});
+
+	
+
+	$(window).on("resize load", function(){
+		if ( $(window).width() > 800 ) {
+			if ( $('.nav-tab-list').hasClass("slick-initialized")) {
+				$('.nav-tab-list').slick('unslick');
+			}
+		}
+		else if ( !($('.nav-tab-list').hasClass("slick-initialized")) ) {
+			$('.nav-tab-list').slick({
+				dots: false,
+				arrows: true,
+				prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left"></i></button>',
+				nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right"></i></button>',
+			});
+		}
+	});
+
+	$('.nav-tab-list').on('afterChange', function(event, slick, currentSlide, nextSlide) {
+		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
+		$(this).find('.slick-current').siblings().removeClass('active');
+		$(this).find('.slick-current').addClass('active');
+		let id = $(this).find('.active').find('.nav-tab-list__link').attr('href')
+		$(id).removeClass('hide');
+		return false
+	})
+
+
 });
 
 // alert(window.screen.width);
